0a3629c4ff47bc3ecf4ab5843101d7986a66aba0
Macuyler <macuyler@gmail.com>
2020-09-10 14:17:40 -0500

    Add Support and Privacy pages

---
+++ src/components/pages/Privacy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivacyPage = () => {
+    return (
+        <div className="Page">
+            <h1>Privacy Policy</h1>
+        </div>
+    );
+};
+
+export default PrivacyPage;
---
+++ src/components/pages/Support.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SupportPage = () => {
+    return (
+        <div className="Page">
+            <h1>Support Page</h1>
+        </div>
+    );
+};
