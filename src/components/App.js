013a06f042c40776a0dcbd4551006f3d481db514
Macuyler <macuyler@gmail.com>
2020-09-10 14:21:20 -0500

    Move banner to HomePage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import HomePage from './pages/Home';
-import Banner from '../assets/img/city.jpeg';
 
 const App = () => {
   return (
     <div className="App">
       <HomePage />
-      <img src={Banner} alt="banner" />
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Banner from '../../assets/img/city.jpeg';
 
 const HomePage = () => {
     return (
-        <div className="Page">
-            <h1>Welcome to the Home Page!</h1>
-        </div>
-    )
+      <div className="Page">
+        <h1>Welcome to the Home Page!</h1>
+        <img src={Banner} alt="banner" />
+      </div>
+    );
 };
 
 export default HomePage;
