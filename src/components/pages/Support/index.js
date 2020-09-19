b85a49841da8c23584831878189915501e2a049f
RiplePro <danielamidon123@gmail.com>
2020-09-18 23:25:48 -0500

    Add support page

--- src/components/pages/Support/index.js
+++ src/components/pages/Support/index.js
@@ -2,11 +2,39 @@ import React from 'react';
 import injectSheet from 'react-jss';
 import PageStyles from '../styles';
 import SupportStyles from './styles';
+import {db} from '../../../firebase';
 
 const SupportPage = ({ classes }) => {
+  const [email, setEmail] = React.useState('');
+  const [content, setContent] = React.useState('');
+  const [status, setStatus] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.length===0 && content.length===0){
+      setStatus('Error: Invalid Input!');
+    }
+    else{
+    db.collection('reports').doc().set({email, content})
+      .then(() => setStatus ('Report Submitted! :D'))
+      .catch((error) => setStatus(`Error: ${error.message}`))
+
+    setEmail('');
+    setContent('');
+    }
+  };
+
   return (
     <div className={classes.page}>
-      <h1>Support Page</h1>
+      <h1 className={classes.h1}>Support Page</h1>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <textarea type="text" placeholder="Need Help?" rows="5" value={content} onChange={e => setContent(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      {status.length > 0 && (
+       <h2 className={status.includes('Error:') ? classes.error : classes.success}>{status}</h2>
+      )}
     </div>
   );
 };
--- src/components/pages/Support/styles.js
+++ src/components/pages/Support/styles.js
@@ -1,3 +1,78 @@
-const SupportStyles = {};
+const SupportStyles = {
+h1: {
+    color: '#fff',
+    fontSize: '75px',
+    marginTop: '100px',
+    backgroundColor: '#03AAEF',
+    borderRadius: '30px',
+    paddingTop: '12px',
+    paddingBottom: '28px',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    margin: '50px auto',
+},
+form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '60%',
+    height: '250px',
+    '& input':{
+        width: '80%',
+        fontSize: '20px',
+        padding: '8px 20px',
+        borderRadius: '30px',
+        outline: 'none',
+        border: 'none',
+        boxSizing: 'border-box',
+    },
+    '& textarea': {
+        width: '80%',
+        fontSize: '20px',
+        padding: '12px 20px',
+        borderRadius: '30px',
+        outline: 'none',
+        border: 'none',
+        resize: 'none',
+        boxSizing: 'border-box',
+    },
+    '& button': {
+        color: '#fff',
+        backgroundColor: '#03AAEF',
+        fontWeight: 'bold',
+        width: '35%',
+        fontSize: '18px',
+        padding: '6px 20px',
+        borderRadius: '30px',
+        outline: 'none',
+        border: 'none',
+    }
+
+},
+
+error: {
+    backgroundColor: 'red',
+    color: '#fff',
+    padding: '6px 24px',
+    borderRadius: '20px',
+    border: 'none',
+    outline: 'none',
+    fontSize: '15px',
+    fontWeight: 'bold',
+    marginTop: '30px',
+},
+success: {
+    backgroundColor: '#60c640',
+    color: '#fff',
+    padding: '6px 24px',
+    borderRadius: '20px',
+    border: 'none',
+    outline: 'none',
+    fontSize: '15px',
+    fontWeight: 'bold',
+    marginTop: '30px',
+},
+};
 
 export default SupportStyles;
