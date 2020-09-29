14d820ced213f5fa9e02efc0ce61b7e97e4fb24e
Macuyler <macuyler@gmail.com>
2020-09-28 20:22:14 -0500

    Add ScrollToTop

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import injectSheet from 'react-jss';
+import ScrollToTop from './utils/ScrollToTop';
 import routes from './pages';
 
 const App = ({ classes }) => {
   return (
     <div className={classes.app}>
       <Router>
+        <ScrollToTop />
         <Switch>
           {Object.keys(routes).map((route) => (
             <Route key={route} path={route} component={routes[route]} />
