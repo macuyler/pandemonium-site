a1a6f39c34b71638834f3352cf44c89bfbe843f0
Macuyler <macuyler@gmail.com>
2020-11-18 11:01:58 -0700

    Add pageInjext method to cleanup page styles reuse

---
+++ src/components/pages/AppStore/styles.js
@@ -0,0 +1,8 @@
+const AppStoreStyles = {
+  h1: {
+    color: '#fff',
+    marginTop: 10,
+  },
+};
+
+export default AppStoreStyles;
---
+++ src/components/pages/inject.js
@@ -0,0 +1,6 @@
+import injectSheet from 'react-jss';
+import PageStyles from './styles';
+
+const pageInject = (styles) => injectSheet({ ...PageStyles, ...styles });
+
+export default pageInject;
