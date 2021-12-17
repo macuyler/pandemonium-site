173a86659390b5e7f285d6dab8aba82059128911
Macuyler Dunn <macuyler@protonmail.com>
2021-12-17 12:06:35 -0600

    Escape all special characters in the privacy policy

--- src/components/pages/Privacy/index.js
+++ src/components/pages/Privacy/index.js
@@ -14,23 +14,26 @@ const PrivacyPage = ({ classes }) => {
           <h2>Last updated October 01, 2020</h2>
           <p>
             Thank you for choosing to be part of our community at
-            Pandemonium2020 ("
-            <B>Company</B>", "<B>we</B>", "<B>us</B>", "<B>our</B>"). We are
-            committed to protecting your personal information and your right to
-            privacy. If you have any questions or concerns about this privacy
-            notice, or our practices with regards to your personal information,
-            please contact us at contact.macuyler@gmail.com.
+            Pandemonium2020 (&quot;
+            <B>Company</B>&quot;, &quot;<B>we</B>&quot;, &quot;<B>us</B>
+            &quot;, &quot;
+            <B>our</B>&quot;). We are committed to protecting your personal
+            information and your right to privacy. If you have any questions or
+            concerns about this privacy notice, or our practices with regards to
+            your personal information, please contact us at
+            contact.macuyler@gmail.com.
           </p>
           <p>
-            When you use our mobile application, as the case may be (the "
+            When you use our mobile application, as the case may be (the &quot;
             <B>App</B>
-            ") and more generally, use any of our services (the "<B>Services</B>
-            ", which include the App), we appreciate that you are trusting us
-            with your personal information. We take your privacy very seriously.
-            In this privacy notice, we seek to explain to you in the clearest
-            way possible what information we collect, how we use it and what
-            rights you have in relation to it. We hope you take some time to
-            read through it carefully, as it is important. If there are any
+            &quot;) and more generally, use any of our services (the &quot;
+            <B>Services</B>
+            &quot;, which include the App), we appreciate that you are trusting
+            us with your personal information. We take your privacy very
+            seriously. In this privacy notice, we seek to explain to you in the
+            clearest way possible what information we collect, how we use it and
+            what rights you have in relation to it. We hope you take some time
+            to read through it carefully, as it is important. If there are any
             terms in this privacy notice that you do not agree with, please
             discontinue use of our Services immediately.
           </p>
@@ -134,8 +137,8 @@ const PrivacyPage = ({ classes }) => {
                 the App (such as the date/time stamps associated with your
                 usage, pages and files viewed, searches and other actions you
                 take such as which features you use), device event information
-                (such as system activity, error reports (sometimes called 'crash
-                dumps') and hardware settings).
+                (such as system activity, error reports (sometimes called
+                &apos;crash dumps&apos;) and hardware settings).
               </li>
             </ul>
           </div>
@@ -189,8 +192,8 @@ const PrivacyPage = ({ classes }) => {
                 when expressing an interest in obtaining information about us or
                 our App, subscribing to marketing or otherwise contacting us, we
                 will collect personal information from you. You can opt-out of
-                our marketing emails at any time (see the "
-                <a href="#seven">WHAT ARE YOUR PRIVACY RIGHTS</a>" below).
+                our marketing emails at any time (see the &quot;
+                <a href="#seven">WHAT ARE YOUR PRIVACY RIGHTS</a>&quot; below).
               </li>
               <li>
                 <B>Deliver targeted advertising to you.</B> We may use your
@@ -268,9 +271,9 @@ const PrivacyPage = ({ classes }) => {
               outside, please be aware that your information may be transferred
               to, stored, and processed by us in our facilities and by those
               third parties with whom we may share your personal information
-              (see "
-              <a href="#three">WILL YOUR INFORMATION BE SHARED WITH ANYONE?</a>"
-              above), in and other countries.
+              (see &quot;
+              <a href="#three">WILL YOUR INFORMATION BE SHARED WITH ANYONE?</a>
+              &quot; above), in and other countries.
             </p>
             <p>
               If you are a resident in the European Economic Area, then these
@@ -364,16 +367,16 @@ const PrivacyPage = ({ classes }) => {
             <h4>8. CONTROLS FOR DO-NOT-TRACK FEATURES</h4>
             <p>
               Most web browsers and some mobile operating systems and mobile
-              applications include a Do-Not-Track ("DNT") feature or setting you
-              can activate to signal your privacy preference not to have data
-              about your online browsing activities monitored and collected. At
-              this stage no uniform technology standard for recognizing and
-              implementing DNT signals has been finalized. As such, we do not
-              currently respond to DNT browser signals or any other mechanism
-              that automatically communicates your choice not to be tracked
-              online. If a standard for online tracking is adopted that we must
-              follow in the future, we will inform you about that practice in a
-              revised version of this privacy notice.
+              applications include a Do-Not-Track (&quot;DNT&quot;) feature or
+              setting you can activate to signal your privacy preference not to
+              have data about your online browsing activities monitored and
+              collected. At this stage no uniform technology standard for
+              recognizing and implementing DNT signals has been finalized. As
+              such, we do not currently respond to DNT browser signals or any
+              other mechanism that automatically communicates your choice not to
+              be tracked online. If a standard for online tracking is adopted
+              that we must follow in the future, we will inform you about that
+              practice in a revised version of this privacy notice.
             </p>
           </div>
           <div id="nine">
@@ -384,16 +387,17 @@ const PrivacyPage = ({ classes }) => {
               information.
             </p>
             <p>
-              California Civil Code Section 1798.83, also known as the "Shine
-              The Light" law, permits our users who are California residents to
-              request and obtain from us, once a year and free of charge,
-              information about categories of personal information (if any) we
-              disclosed to third parties for direct marketing purposes and the
-              names and addresses of all third parties with which we shared
-              personal information in the immediately preceding calendar year.
-              If you are a California resident and would like to make such a
-              request, please submit your request in writing to us using the
-              contact information provided below.
+              California Civil Code Section 1798.83, also known as the
+              &quot;Shine The Light&quot; law, permits our users who are
+              California residents to request and obtain from us, once a year
+              and free of charge, information about categories of personal
+              information (if any) we disclosed to third parties for direct
+              marketing purposes and the names and addresses of all third
+              parties with which we shared personal information in the
+              immediately preceding calendar year. If you are a California
+              resident and would like to make such a request, please submit your
+              request in writing to us using the contact information provided
+              below.
             </p>
             <p>
               If you are under 18 years of age, reside in California, and have a
@@ -416,13 +420,13 @@ const PrivacyPage = ({ classes }) => {
             </p>
             <p>
               We may update this privacy notice from time to time. The updated
-              version will be indicated by an updated "Revised" date and the
-              updated version will be effective as soon as it is accessible. If
-              we make material changes to this privacy notice, we may notify you
-              either by prominently posting a notice of such changes or by
-              directly sending you a notification. We encourage you to review
-              this privacy notice frequently to be informed of how we are
-              protecting your information.
+              version will be indicated by an updated &quot;Revised&quot; date
+              and the updated version will be effective as soon as it is
+              accessible. If we make material changes to this privacy notice, we
+              may notify you either by prominently posting a notice of such
+              changes or by directly sending you a notification. We encourage
+              you to review this privacy notice frequently to be informed of how
+              we are protecting your information.
             </p>
           </div>
           <div id="eleven">
