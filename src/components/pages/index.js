fca935e9213ed2ae2aa01bb659afb30863ba444d
Macuyler Dunn <dev@macuyler.com>
2024-11-24 10:25:39 -0500

    Remove support page and dependencies

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,10 +1,8 @@
 import HomePage from "./Home";
 import PrivacyPage from "./Privacy";
-import SupportPage from "./Support";
 
 const routes = {
   "/privacy": PrivacyPage,
-  "/support": SupportPage,
   "/": HomePage,
 };
 
